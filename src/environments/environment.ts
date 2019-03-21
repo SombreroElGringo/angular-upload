9e9356ed3e5ef3138d37a52fbc82f692def84b6e
SombreroElGringo <florent.pailhes@hotmail.fr>
2019-03-21 20:38:00 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false
 };
 
-export const FIREBASE_API_KEY = "AIzaSyCXv2Ej7uwF_S4BFGYvscbs1SYV-0zkunE";
-export const FIREBASE_AUTH_DOMAIN = "angular-uploads.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://angular-uploads.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "angular-uploads";
-export const FIREBASE_STORAGE_BUCKET = "angular-uploads.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "992678420518";
+export const FIREBASE_API_KEY = null;
+export const FIREBASE_AUTH_DOMAIN = null;
+export const FIREBASE_DATABASE_URL = null;
+export const FIREBASE_PROJECT_ID = null;
+export const FIREBASE_STORAGE_BUCKET = null;
+export const FIREBASE_MESSAGING_SENDER_ID = null;
 
 export const FIREBASE_CONFIG = {
   apiKey: FIREBASE_API_KEY,
